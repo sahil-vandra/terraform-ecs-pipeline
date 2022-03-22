d3c9aa147afbaeb775999731528c0a2526bd2537
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-03-22 10:55:44 +0530

    22 march 1st commit

--- script.sh
+++ script.sh
@@ -3,12 +3,17 @@
 AWS_ACCOUNT_ID="997817439961"
 AWS_DEFAULT_REGION="ap-south-1" 
 IMAGE_REPO_NAME="sahil-demo"
-IMAGE_TAG="prod-img_${GIT_COMMIT}"
-CLUSTER_NAME="Production-Cluster"
-SERVICE_NAME="Prod-Service"
-TASK_DEFINITION_NAME="Production"
+IMAGE_TAG="terraform-ecs-pipeline-img_${GIT_COMMIT}"
+CLUSTER_NAME="Sahil-Demo-Cluster"
+SERVICE_NAME="Sahil-Demo-Service"
+TASK_DEFINITION_NAME="Sahil-Demo-TaskDefinition"
 DESIRED_COUNT="1"
 ECR_IMAGE="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}"
+
+pwd 
+
+# create infrastructure by terraform script
+terraform apply -auto-approve
 
 # login in to aws ecr
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 997817439961.dkr.ecr.ap-south-1.amazonaws.com
@@ -17,11 +22,9 @@ aws ecr get-login-password --region ap-south-1 | docker login --username AWS --p
 docker build -t ${IMAGE_TAG} .
 
 # tag image
-#docker tag ${IMAGE_TAG}:latest 997817439961.dkr.ecr.ap-south-1.amazonaws.com/sahil-demo:${IMAGE_TAG}
 docker tag ${IMAGE_TAG}:latest ${ECR_IMAGE}
 
 # push image in aws ecr
-#docker push 997817439961.dkr.ecr.ap-south-1.amazonaws.com/sahil-demo:${IMAGE_TAG}
 docker push ${ECR_IMAGE}
 
 # get role arn store in variable 
@@ -29,11 +32,9 @@ ROLE_ARN=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_
 
 # get family store in variable 
 FAMILY=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.family`
-#echo "FAMILY= " $FAMILY
 
-# get name arn store in variable 
+# get name store in variable 
 NAME=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.containerDefinitions[].name`
-#echo "NAME= " $NAME
 
 # find and replace some content in task-definition file
 sed -i "s#BUILD_NUMBER#$ECR_IMAGE#g" task-definition.json
@@ -60,4 +61,3 @@ fi
 
 # update servise
 aws ecs update-service --region ap-south-1 --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}" --force-new-deployment 
-
