363ab022a54a5540c58c86811ba5016a86662088
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-03-22 15:55:43 +0530

    22 march 8th commit

--- script.sh
+++ script.sh
@@ -47,18 +47,18 @@ sed -i "s#NAME#$NAME#g" task-definition.json
 # Get task definition from the aws console
 TASK_DEF_REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_DEFAULT_REGION}" | jq .taskDefinition.revision`
 
-# TASK_DEF_REVISION=$((TASK_DEF_REVISION-4))
+TASK_DEF_REVISION=$((TASK_DEF_REVISION-4))
 
 cat task-definition.json
 
 # register new task definition from new generated task definition file
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
 
-# if [ $TASK_DEF_REVISION>0 ]
-# then
+if [ $TASK_DEF_REVISION>0 ]
+then
 	# deregister previous task definiiton
 	aws ecs deregister-task-definition --region ap-south-1 --task-definition Production:${TASK_DEF_REVISION}
-# fi
+fi
 
 # update servise
 aws ecs update-service --region ap-south-1 --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}" --force-new-deployment 
